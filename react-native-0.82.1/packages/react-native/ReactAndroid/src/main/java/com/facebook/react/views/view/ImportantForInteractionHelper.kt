d85b68b36d6ad488e802ee6f8c14690c76b08f33
liangkan123 <877453297@qq.om>
2026-01-29 11:08:21 +0800

    Add react-native-0.82.1 files

---
+++ react-native-0.82.1/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ImportantForInteractionHelper.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.views.view
+
+import android.view.View
+import com.facebook.react.R
+import com.facebook.react.uimanager.PointerEvents
+
+/**
+ * Helper class for managing the important_for_interaction view tag. This tag determines how a view
+ * participates in interaction handling.
+ *
+ * The important_for_interaction value is a bitfield that can contain combinations of:
+ * - [IMPORTANT_FOR_INTERACTION_YES]: The view is important for interaction
+ * - [IMPORTANT_FOR_INTERACTION_NO]: The view is not important for interaction
+ * - [IMPORTANT_FOR_INTERACTION_EXCLUDE_DESCENDANTS]: Descendants should be excluded from
+ *   interaction
+ */
+internal object ImportantForInteractionHelper {
+  /** The view is important for interaction. */
+  const val IMPORTANT_FOR_INTERACTION_YES: Int = 0x1
+
+  /** The view is not important for interaction. */
+  const val IMPORTANT_FOR_INTERACTION_NO: Int = 0x2
+
+  /** Descendants of this view should be excluded from interaction handling. */
+  const val IMPORTANT_FOR_INTERACTION_EXCLUDE_DESCENDANTS: Int = 0x4
+
+  /**
+   * Sets the important_for_interaction tag on a view based on the given [PointerEvents] value.
+   *
+   * Note: The pointer events value alone does not determine if a view is important for interaction.
+   * A view with pointerEvents=auto or pointerEvents=box-only is only important for interaction if
+   * it is also clickable. Therefore, for these cases we let the view's own state determine whether
+   * it's important for interaction rather than setting the tag.
+   *
+   * The mapping is as follows:
+   * - [PointerEvents.AUTO] -> Tag removed (i.e. the view's own state determines importance)
+   * - [PointerEvents.NONE] -> [IMPORTANT_FOR_INTERACTION_NO] |
+   *   [IMPORTANT_FOR_INTERACTION_EXCLUDE_DESCENDANTS]
+   * - [PointerEvents.BOX_ONLY] -> [IMPORTANT_FOR_INTERACTION_EXCLUDE_DESCENDANTS] (i.e. the view's
+   *   own state determines importance but its descendants are excluded)
+   * - [PointerEvents.BOX_NONE] -> [IMPORTANT_FOR_INTERACTION_NO]
+   *
+   * @param view The view to set the tag on
+   * @param pointerEvents The pointer events value to convert and set
+   */
+  @JvmStatic
+  fun setImportantForInteraction(view: View, pointerEvents: PointerEvents) {
+    val value =
+        when (pointerEvents) {
+          PointerEvents.AUTO -> null
+          PointerEvents.NONE ->
+              IMPORTANT_FOR_INTERACTION_NO or IMPORTANT_FOR_INTERACTION_EXCLUDE_DESCENDANTS
+          PointerEvents.BOX_ONLY -> IMPORTANT_FOR_INTERACTION_EXCLUDE_DESCENDANTS
+          PointerEvents.BOX_NONE -> IMPORTANT_FOR_INTERACTION_NO
+        }
+    view.setTag(R.id.important_for_interaction, value)
+  }
+}
---
+++ react-native-0.82.1/packages/react-native/src/private/featureflags/specs/NativeReactNativeFeatureFlags.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<deac1f5897eec35b6b1c503ed9e15d77>>
+ * @flow strict
+ * @noformat
+ */
+
+/**
+ * IMPORTANT: Do NOT modify this file directly.
+ *
+ * To change the definition of the flags, edit
+ *   packages/react-native/scripts/featureflags/ReactNativeFeatureFlags.config.js.
+ *
+ * To regenerate this code, run the following script from the repo root:
+ *   yarn featureflags --update
+ */
+
+import type {TurboModule} from '../../../../Libraries/TurboModule/RCTExport';
+
+import * as TurboModuleRegistry from '../../../../Libraries/TurboModule/TurboModuleRegistry';
+
+export interface Spec extends TurboModule {
+  +commonTestFlag?: () => boolean;
+  +commonTestFlagWithoutNativeImplementation?: () => boolean;
+  +cdpInteractionMetricsEnabled?: () => boolean;
+  +cxxNativeAnimatedEnabled?: () => boolean;
+  +disableEarlyViewCommandExecution?: () => boolean;
+  +disableImageViewPreallocationAndroid?: () => boolean;
+  +disableMountItemReorderingAndroid?: () => boolean;
+  +disableSubviewClippingAndroid?: () => boolean;
+  +disableTextLayoutManagerCacheAndroid?: () => boolean;
+  +disableViewPreallocationAndroid?: () => boolean;
+  +enableAccessibilityOrder?: () => boolean;
+  +enableAccumulatedUpdatesInRawPropsAndroid?: () => boolean;
+  +enableAndroidAntialiasedBorderRadiusClipping?: () => boolean;
+  +enableAndroidLinearText?: () => boolean;
+  +enableAndroidTextMeasurementOptimizations?: () => boolean;
+  +enableBridgelessArchitecture?: () => boolean;
+  +enableCppPropsIteratorSetter?: () => boolean;
+  +enableCustomFocusSearchOnClippedElementsAndroid?: () => boolean;
+  +enableDestroyShadowTreeRevisionAsync?: () => boolean;
+  +enableDoubleMeasurementFixAndroid?: () => boolean;
+  +enableEagerMainQueueModulesOnIOS?: () => boolean;
+  +enableEagerRootViewAttachment?: () => boolean;
+  +enableExclusivePropsUpdateAndroid?: () => boolean;
+  +enableFabricLogs?: () => boolean;
+  +enableFabricRenderer?: () => boolean;
+  +enableFontScaleChangesUpdatingLayout?: () => boolean;
+  +enableIOSTextBaselineOffsetPerLine?: () => boolean;
+  +enableIOSViewClipToPaddingBox?: () => boolean;
+  +enableImagePrefetchingAndroid?: () => boolean;
+  +enableImagePrefetchingJNIBatchingAndroid?: () => boolean;
+  +enableImagePrefetchingOnUiThreadAndroid?: () => boolean;
+  +enableImmediateUpdateModeForContentOffsetChanges?: () => boolean;
+  +enableImperativeFocus?: () => boolean;
+  +enableInteropViewManagerClassLookUpOptimizationIOS?: () => boolean;
+  +enableIntersectionObserverByDefault?: () => boolean;
+  +enableKeyEvents?: () => boolean;
+  +enableLayoutAnimationsOnAndroid?: () => boolean;
+  +enableLayoutAnimationsOnIOS?: () => boolean;
+  +enableMainQueueCoordinatorOnIOS?: () => boolean;
+  +enableModuleArgumentNSNullConversionIOS?: () => boolean;
+  +enableNativeCSSParsing?: () => boolean;
+  +enableNetworkEventReporting?: () => boolean;
+  +enablePreparedTextLayout?: () => boolean;
+  +enablePropsUpdateReconciliationAndroid?: () => boolean;
+  +enableSwiftUIBasedFilters?: () => boolean;
+  +enableViewCulling?: () => boolean;
+  +enableViewRecycling?: () => boolean;
+  +enableViewRecyclingForImage?: () => boolean;
+  +enableViewRecyclingForScrollView?: () => boolean;
+  +enableViewRecyclingForText?: () => boolean;
+  +enableViewRecyclingForView?: () => boolean;
+  +enableVirtualViewContainerStateExperimental?: () => boolean;
+  +enableVirtualViewDebugFeatures?: () => boolean;
+  +fixMappingOfEventPrioritiesBetweenFabricAndReact?: () => boolean;
+  +fixTextClippingAndroid15useBoundsForWidth?: () => boolean;
+  +fuseboxAssertSingleHostState?: () => boolean;
+  +fuseboxEnabledRelease?: () => boolean;
+  +fuseboxNetworkInspectionEnabled?: () => boolean;
+  +hideOffscreenVirtualViewsOnIOS?: () => boolean;
+  +overrideBySynchronousMountPropsAtMountingAndroid?: () => boolean;
+  +perfIssuesEnabled?: () => boolean;
+  +perfMonitorV2Enabled?: () => boolean;
+  +preparedTextCacheSize?: () => number;
+  +preventShadowTreeCommitExhaustion?: () => boolean;
+  +shouldPressibilityUseW3CPointerEventsForHover?: () => boolean;
+  +shouldTriggerResponderTransferOnScrollAndroid?: () => boolean;
+  +skipActivityIdentityAssertionOnHostPause?: () => boolean;
+  +traceTurboModulePromiseRejectionsOnAndroid?: () => boolean;
+  +updateRuntimeShadowNodeReferencesOnCommit?: () => boolean;
+  +useAlwaysAvailableJSErrorHandling?: () => boolean;
+  +useFabricInterop?: () => boolean;
+  +useNativeViewConfigsInBridgelessMode?: () => boolean;
+  +useNestedScrollViewAndroid?: () => boolean;
+  +useSharedAnimatedBackend?: () => boolean;
+  +useTraitHiddenOnAndroid?: () => boolean;
+  +useTurboModuleInterop?: () => boolean;
+  +useTurboModules?: () => boolean;
+  +viewCullingOutsetRatio?: () => number;
+  +virtualViewPrerenderRatio?: () => number;
+}
+
+const NativeReactNativeFeatureFlags: ?Spec = TurboModuleRegistry.get<Spec>(
+  'NativeReactNativeFeatureFlagsCxx',
+);
+
+export default NativeReactNativeFeatureFlags;
